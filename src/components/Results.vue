32423ab5f19f87c4e5c0ecccab400d7ec1d58f4b
Issifou Issa <chrisissifou@gmail.com>
2021-12-11 18:03:32 +0100

    Envoi du projet sur github

---
+++ src/components/Results.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+      <p>Reaction time : {{ score }}</p> 
+       <p class="rank"> {{ rank }}</p> 
+    </div>
+</template>
+
+<script>
+export default {
+
+    props: ['score'],
+
+    data() {
+        return {
+            rank: null
+        }
+    },
+    mounted() {
+        if(this.score < 400) {
+            this.rank = 'Ninja Fingers'
+        } else if (this.score < 600) {
+            this.rank = 'Rapid Reflexes'
+        } else {
+            this.rank = 'Snail pace...'
+        }
+    }
+
+}
+</script>
+
+<style>
+    .rank {
+        font-size: 1.4em;
+        color:#0faf87;
+        font-weight: bold;
+    }
+</style>
+
